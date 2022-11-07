df7b80d2a0a9a1d7b02690073dfa847f24f0e5aa
Devashish Sharma <78838771+DevashishSharma48@users.noreply.github.com>
2022-11-07 14:17:31 +0530

    Add files via upload

---
+++ Runner_up.py
@@ -0,0 +1,10 @@
+//Second/Runner-up Score!
+if __name__ == '__main__':
+    n = int(input())
+    arr = map(int, input().split())
+    
+    new_list=set(arr)
+    
+    new_list.remove(max(new_list))
+    
+    print(max(new_list))
